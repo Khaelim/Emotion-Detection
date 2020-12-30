f2f2db085b88a2e0b433778aec71076828b9872f
KhaeLim <khaelimr@gmail.com>
2020-12-30 15:54:52 +0000

    Proper Face Emotion Detection (Low accuracy)

--- Face Detection/Facial detection/ReFresh.py
+++ Face Detection/Facial detection/ReFresh.py
@@ -2,17 +2,27 @@ import cv2
 import numpy as np
 import tensorflow as tf
 
+# def get_class_string_from_index(index):
+#    for class_string, class_index in valid_generator.class_indices.items():
+#       if class_index == index:
+#          return class_string
+
+class_names=['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise']
+
 mymodel = tf.keras.models.load_model('C:/Khaelim/ForProgramming/TFmodels/my_model.h5', compile=True)
 mymodel.compile(tf.keras.optimizers.Adam(), loss='mse')
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-cap = cv2.VideoCapture('D:\Khaelim\Documents\ProgrammingProjects\Facial detection V2\Soul-Mates.AU.S01E01.WEB-DLx264-JIVE.mp4')
+cap = cv2.VideoCapture(0)
 
-while (cap.isOpened()):
+while (True):
     ret, frame = cap.read()
     if not ret:
         continue
+
+    #cv2.imshow("yoop", frame)
+
 
     img = frame
     temp_img = np.zeros([48,48,3],dtype=np.uint8)
@@ -23,15 +33,38 @@ while (cap.isOpened()):
         roi_gray = frame[y:y + h, x:x + w]
         roi_color = frame[y:y + h, x:x + w]
         temp_img = frame[y:y + h, x:x + w]
-    temp_img1 = tf.image.resize(temp_img, [48,48], preserve_aspect_ratio=True)
 
-    print(temp_img1.get_shape())
-
+    temp_img1 = tf.image.resize(temp_img, [48, 48], preserve_aspect_ratio=True)
     resize_image = tf.reshape(temp_img1, [-1, 48, 48, 3])
 
-    cv2.imshow("yoop", img)
-    print(mymodel.predict(resize_image))
-    if cv2.waitKey(5) & 0xFF == ord('q'):
+    predictions = mymodel.predict(resize_image)
+    cv2.putText(img, str(class_names[np.argmax(predictions[0])]), (x - 10, y - 10), cv2.FONT_HERSHEY_PLAIN, 2, (0, 255, 0))
+
+
+    scale_percent = 75 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+
+    #make prediction
+    predictions = mymodel.predict(resize_image)
+
+    print(str("Prediction Array: "))
+    print(predictions[0])
+    print(str("Prediction No: "))
+    print(np.argmax(predictions[0]))
+
+    pred = str(class_names[np.argmax(predictions[0])])
+
+    # resize image
+    resized = cv2.resize(img, dim, interpolation=cv2.INTER_AREA)
+
+    cv2.imshow("yoop", resized)
+
+    #print(mymodel.summary())
+
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
 cv2.destroyAllWindows()
