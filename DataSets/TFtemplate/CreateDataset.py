29c62f5851321e6df1fe929e3092c7b903566786
Khaelim <Khaelimkman@hotmail.com>
2021-08-17 17:27:18 +0100

    Changes to reflect new filesystem

--- DataSets/TFtemplate/CreateDataset.py
+++ DataSets/TFtemplate/CreateDataset.py
@@ -1,4 +1,4 @@
-#%load_ext tensorboard
+# %load_ext tensorboard
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -13,36 +13,35 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
-dataset_dir = path = os.path.join('C:/Khaelim/ForProgramming/TFdatasets/', "saved_test_data")
-main_dir = 'D:/Khaelim/Documents/ProgrammingProjects/Emotion-Detection/DataSets/Tests'
-train_dir = 'C:/Khaelim/ForProgramming/FERv1/FER13/train/'
-#test_dir = 'D:\Khaelim\Documents\ProgrammingProjects\Emotion-Detection\DataSets\Tests'
+dataset_dir = path = os.path.join('C:/Users/Khaelim/Python Projects/Datasets/IMAGE_FINAL/', "saved_test_data")
+main_dir = 'C:/Users/Khaelim/Python Projects/Datasets/IMAGE_FINAL/'
+train_dir = 'C:/Users/Khaelim/Python Projects/Datasets/IMAGE_FINAL/'
+# test_dir = 'D:\Khaelim\Documents\ProgrammingProjects\Emotion-Detection\DataSets\Tests'
 main_dir = pathlib.Path(main_dir)
 
-train_dir = pathlib.Path(train_dir)
-image_count = len(list(train_dir.glob('*/*.jpg')))
+main_dir = pathlib.Path(main_dir)
+image_count = len(list(main_dir.glob('*/*.jpg')))
 
-angry = list(train_dir.glob('angry/*'))
-disgust = list(train_dir.glob('disgust/*'))
-fear = list(train_dir.glob('fear/*'))
-happy = list(train_dir.glob('happy/*'))
-neutral = list(train_dir.glob('neutral/*'))
-sad = list(train_dir.glob('sad/*'))
-surprise = list(train_dir.glob('surprise/*'))
+angry = list(main_dir.glob('anger/*'))
+disgust = list(main_dir.glob('disgust/*'))
+fear = list(main_dir.glob('fear/*'))
+happy = list(main_dir.glob('happy/*'))
+neutral = list(main_dir.glob('neutral/*'))
+sad = list(main_dir.glob('sad/*'))
+surprise = list(main_dir.glob('surprise/*'))
 
 batch_size = 32
 img_height = 48
 img_width = 48
 
-
 main_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  train_dir,
-  labels='inferred',
-  color_mode='grayscale',
-  class_names=['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'],
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+    main_dir,
+    labels='inferred',
+    color_mode='grayscale',
+    class_names=['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'],
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
 
 # train_ds = tf.keras.preprocessing.image_dataset_from_directory(
 #   train_dir,
@@ -67,14 +66,13 @@ main_ds = tf.keras.preprocessing.image_dataset_from_directory(
 #   batch_size=batch_size)
 
 
-tf.data.experimental.save(main_ds, main_dir)
+tf.data.experimental.save(main_ds, dataset_dir)
 # tf.data.experimental.save(train_ds, main_dir)
 # tf.data.experimental.save(test_ds, main_dir)
 
 
-
-#To cache the model for repeated training
-#test_ds = train_ds.cache().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
+# To cache the model for repeated training
+# test_ds = train_ds.cache().prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
 
 
 print('Done')
--- Model/ModelTest/CreateModel.py
+++ Model/ModelTest/CreateModel.py
@@ -16,80 +16,80 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 print(tf.__version__)
-train_dir = 'C:\Khaelim\ForProgramming\FERv1\FER13\\full'
-test_dir = 'C:\Khaelim\ForProgramming\FERv1\FER13\\test'
-train_dir = pathlib.Path(train_dir)
-test_dir = pathlib.Path(test_dir)
 
-
-# path = os.path.join('C:\Khaelim\Documents\ProgrammingProjects\Emotion-Detection\models\\', "saved_data")
-# dataset = tf.data.Dataset.range(2)
-# tf.data.experimental.save(dataset, path)
-# main_dataset = tf.data.experimental.load(path, tf.TensorSpec(shape=([48,48]), dtype=tf.float32))
-
-#image_count = len(list(train_dir.glob('*/*.jpg')))
-
-batch_size = 32
-img_height = 48
-img_width = 48
-
+"""Load test, Currently performed below"""
 # train_ds=tf.data.experimental.load(train_dir,
 #     tf.TensorSpec(shape=([48,48]), dtype=tf.float32))
 #
 # test_ds=tf.data.experimental.load(test_dir,
 #     tf.TensorSpec(shape=([48,48]), dtype=tf.float32))
 
-main_dir = 'C:/Khaelim/ForProgramming/FERv1/FER13/full/'
-train_dir = 'C:/Khaelim/ForProgramming/FERv1/FER13/train/'
-test_dir = 'C:/Khaelim/ForProgramming/FERv1/FER13/test/'
-#test_dir = 'D:\Khaelim\Documents\ProgrammingProjects\Emotion-Detection\DataSets\Tests'
+dataset_dir = path = os.path.join('C:/Users/Khaelim/Python Projects/Datasets/IMAGE_FINAL/', "saved_test_data")
+main_dir = 'C:/Users/Khaelim/Python Projects/Datasets/IMAGE_FINAL/'
 main_dir = pathlib.Path(main_dir)
 
-train_dir = pathlib.Path(train_dir)
-test_dir = pathlib.Path(test_dir)
-image_count = len(list(train_dir.glob('*/*.jpg')))
+# train_dir = 'C:/Khaelim/ForProgramming/FERv1/FER13/train/'
+# test_dir = 'C:/Khaelim/ForProgramming/FERv1/FER13/test/'
+# test_dir = 'D:\Khaelim\Documents\ProgrammingProjects\Emotion-Detection\DataSets\Tests'
 
-angry = list(train_dir.glob('angry/*'))
-disgust = list(train_dir.glob('disgust/*'))
-fear = list(train_dir.glob('fear/*'))
-happy = list(train_dir.glob('happy/*'))
-neutral = list(train_dir.glob('neutral/*'))
-sad = list(train_dir.glob('sad/*'))
-surprise = list(train_dir.glob('surprise/*'))
+# train_dir = pathlib.Path(train_dir)
+# test_dir = pathlib.Path(test_dir)
+# image_count = len(list(main_dir.glob('*/*.jpg')))
 
-batch_size = 32
-img_height = 48
-img_width = 48
+# angry = list(main_dir.glob('anger/*'))
+# disgust = list(main_dir.glob('disgust/*'))
+# fear = list(main_dir.glob('fear/*'))
+# happy = list(main_dir.glob('happy/*'))
+# neutral = list(main_dir.glob('neutral/*'))
+# sad = list(main_dir.glob('sad/*'))
+# surprise = list(main_dir.glob('surprise/*'))
 
+"""Creating dataset in done separately"""
+# main_ds = tf.keras.preprocessing.image_dataset_from_directory(
+#   main_dir,
+#   labels='inferred',
+#   color_mode='grayscale',
+#   class_names=['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'],
+#   seed=123,
+#   image_size=(img_height, img_width),
+#   batch_size=batch_size)
 
-train_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  train_dir,
-  labels='inferred',
-  color_mode='grayscale',
-  class_names=['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'],
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
+# train_ds = tf.keras.preprocessing.image_dataset_from_directory(
+#   train_dir,
+#   labels='inferred',
+#   color_mode='grayscale',
+#   class_names=['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'],
+#   seed=123,
+#   image_size=(img_height, img_width),
+#   batch_size=batch_size)
+#
+#
+# test_ds = tf.keras.preprocessing.image_dataset_from_directory(
+#   test_dir,
+#   labels='inferred',
+#   color_mode='grayscale',
+#   class_names=['anger', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'],
+#   seed=123,
+#   image_size=(img_height, img_width),
+#   batch_size=batch_size)
 
-
-test_ds = tf.keras.preprocessing.image_dataset_from_directory(
-  test_dir,
-  labels='inferred',
-  color_mode='grayscale',
-  class_names=['angry', 'disgust', 'fear', 'happy', 'neutral', 'sad', 'surprise'],
-  seed=123,
-  image_size=(img_height, img_width),
-  batch_size=batch_size)
-
-class_names = train_ds.class_names
-print(class_names)
-print(len(train_ds.class_names))
+# class_names = train_ds.class_names
+# print(class_names)
+# print(len(train_ds.class_names))
 
 #caching images to increase performance
+# AUTOTUNE = tf.data.experimental.AUTOTUNE
+
+# train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
+# train_ds = train_dir.cache().prefetch(buffer_size=AUTOTUNE)
+
+"""Loading the dataset"""
+main_dataset = tf.data.experimental.load(path)
+"""Caching the dataset"""
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
-train_ds = train_ds.cache().prefetch(buffer_size=AUTOTUNE)
-#train_ds = train_dir.cache().prefetch(buffer_size=AUTOTUNE)
+main_dataset = main_dataset.cache().prefetch(buffer_size=AUTOTUNE)
+
 
 #number of clasifications
 num_classes = 7
@@ -99,11 +99,11 @@ model = tf.keras.Sequential([
   layers.Dense(6, input_shape=(48, 48, 1)),
   layers.Conv2D(32, 3, activation='relu'),
   layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, activation='softmax'),
+  layers.Conv2D(32, 3, activation='relu'),
   layers.MaxPooling2D(),
   layers.Conv2D(32, 3, activation='relu'),
   layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, activation='elu'),
+  layers.Conv2D(32, 3, activation='relu'),
   layers.Flatten(),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
@@ -115,26 +115,31 @@ model.compile(
   metrics=['accuracy'])
 
 #Attempting to add tensorboard
-log_dir = "C:\Khaelim\ForProgramming\TBlogs" #+ datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+log_dir = "C:/Khaelim/ForProgramming/TBlogs" #+ datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
 
 #training the model in loop w/ checkpoint
 
 model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-    filepath='D:\Khaelim\Documents\ProgrammingProjects\Emotion-Detection\checkpooints',
+    filepath='C:/Khaelim/ForProgramming/checkpooints',
     save_weights_only=True,
     monitor='val_acc',
     mode='max',
     save_best_only=True)
 
 
+batch_size = 128
+img_height = 48
+img_width = 48
+epochs = 100
+
 model.fit(
-  train_ds,
-  validation_data=test_ds,
-  batch_size=128,
+  main_dataset,
+  #validation_data=test_ds,
+  batch_size=batch_size,
   steps_per_epoch=None,
-  epochs=35,
+  epochs=epochs,
   callbacks=[tensorboard_callback])
 
 #
@@ -145,4 +150,4 @@ tf.keras.models.save_model(
     signatures=None, options=None
 )
 
-model.save('my_model.h5')
+model.save('my_model_v2.h5')
